9c85d0791d6469446bc5ce92b8198679d1aa71dc
artemmikh <artem.mikh100@gmail.com>
2024-01-13 22:17:57 +0300

    media + "/"

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # MEDIA_ROOT = '/media'
 
 AUTH_USER_MODEL = 'users.MyUser'
